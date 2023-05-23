fa5a06020fcbf60f89f5bdf2f53224dd23268d7d
Wonho Lim <62059099+wonholim02@users.noreply.github.com>
2023-05-23 10:37:31 -0400

    simple change

--- Algorithms/Graph Algorithm/DisjointSetNode.java
+++ Algorithms/Graph Algorithm/DisjointSetNode.java
@@ -3,11 +3,23 @@ public class DisjointSetNode<T> {
     private DisjointSetNode<T> parent;
     private T data;
     private int rank;
-
+    
     public DisjointSetNode(T data) {
         this.parent = this;
         this.data = data;
         this.rank = 0;
+    }
+    
+    public void setParent(DisjointSetNode<T> parent) {
+        this.parent = parent;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
     }
 
     public DisjointSetNode<T> getParent() {
@@ -20,18 +32,6 @@ public class DisjointSetNode<T> {
 
     public int getRank() {
         return rank;
-    }
-
-    public void setParent(DisjointSetNode<T> parent) {
-        this.parent = parent;
-    }
-
-    public void setData(T data) {
-        this.data = data;
-    }
-
-    public void setRank(int rank) {
-        this.rank = rank;
     }
 
     @Override
